6aa687b62b43a12aad3e86c7e99689144c4cf3a3
Guilherme <gui.mp8@gmail.com>
2025-01-19 14:40:33 -0300

    v

--- start.sh
+++ start.sh
@@ -1,1 +1,7 @@
-java -jar jars/selenium-server-4.27.0.jar standalone & python3 src/main.py
+#!/bin/bash
+
+# Run Selenium server in the background
+java -jar jars/selenium-server-4.27.0.jar standalone &
+
+# Run the Python application
+python3 src/main.py
