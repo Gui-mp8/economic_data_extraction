7811296088e0aae0d4a78dfd226807282c2b40c7
Guilherme <gui.mp8@gmail.com>
2025-01-19 22:35:12 -0300

    fix: updating teh selenium extraction to run in only one container.

--- src/main.py
+++ src/main.py
@@ -7,14 +7,14 @@ from strategies.selenium_investing import SeleniumInvestingStrategy
 from targets.cloud_storage import CloudStorage
 
 def main(config):
-    print("hello World")
-    # today = datetime.now().strftime('%Y-%m-%d')
+    today = datetime.now().strftime('%Y-%m-%d')
 
-    # investing = SeleniumInvestingStrategy()
+    investing = SeleniumInvestingStrategy()
 
-    # investing.url = f"https://api.investing.com/api/financialdata/historical/948434?start-date=1991-01-01&end-date={today}&time-frame=Monthly&add-missing-rows=false"
-    # bloomberg = Scraper(scraper_strategy=investing)
-    # bloomberg_data = bloomberg.data()
+    investing.url = f"https://api.investing.com/api/financialdata/historical/948434?start-date=1991-01-01&end-date={today}&time-frame=Monthly&add-missing-rows=false"
+    bloomberg = Scraper(scraper_strategy=investing)
+    bloomberg_data = bloomberg.data()
+    
     # CloudStorage(config).upload_json(bloomberg_data, "bloomberg.json")
 
     # investing.url = f"https://api.investing.com/api/financialdata/historical/2111?start-date=1991-01-01&end-date={today}&time-frame=Monthly&add-missing-rows=false"
