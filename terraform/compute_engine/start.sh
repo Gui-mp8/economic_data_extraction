648ac1f77346ecb581d316ecf4a6e05ed3c07baa
Guilherme Machado <gui.mp@gmail.com>
2025-01-22 17:08:38 -0300

    .

--- terraform/compute_engine/start.sh
+++ terraform/compute_engine/start.sh
@@ -6,15 +6,16 @@ sudo apt -y install make > output.txt
 # install docker and waiting until it is installed
 sudo snap install docker >> output.txt
 sudo snap refresh docker --channel=latest/edge
+
 while ! docker info > /dev/null 2>&1; do
-    echo "Aguardando a inicialização do Docker..."
+    echo "Waiting Docker Inicialization..."
     sleep 1
 done
 
 curl -sSL https://install.astronomer.io | sudo bash
 
 # principal folder
-cd economic_data_extraction/airflow
+cd economic_data_extraction
 
 # astro config
 
